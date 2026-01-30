bbf63c7fd0103603b1cbff741d09bea15f6c490a
yanyao <esc@linux.do>
2026-01-30 15:55:52 +0800

    update

--- generate.sh
+++ generate.sh
@@ -1,6 +1,3 @@
 
 # 生成 dist 文件
 npm run build
-
-# 移动总内容
-cp src/artalk.json dist
--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ imagePathList.forEach((item) => {
 
   const artalk = generateUtils.artalk(imageList, item)
   writeFile(`artalk`, item.path, JSON.stringify(artalk))
+  // collect link for combined artalk (use baseUrl if provided, otherwise relative path)
+  let artalkLink = ''
+  if (item.baseUrl) {
+    artalkLink = item.baseUrl.replace(/\/image\/?$/, '') + '/artalk.json'
+  } else {
+    artalkLink = `./${item.path}/artalk.json`
+  }
+  // push link string
+  if (typeof global.combinedArtalk === 'undefined') global.combinedArtalk = []
+  global.combinedArtalk.push(artalkLink)
 
   const valine = generateUtils.valine(imageList, item)
   writeFile(`valine`, item.path, JSON.stringify(valine))
@@ -23,3 +33,11 @@ imagePathList.forEach((item) => {
 
   copyDir(`${item.path}`, `${item.path}`)
 })
+
+// 写入总的 artalk.json 到 dist 根目录，格式为链接数组，包含每个分组的 artalk.json 地址
+try {
+  const list = global.combinedArtalk || []
+  writeFile(`artalk`, ``, JSON.stringify(list, null, 2))
+} catch (e) {
+  console.error('write combined artalk error', e)
+}
